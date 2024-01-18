9d058d31b0bb4c7bb4ca6df7dd6c826be60cbd69
clau <claudmatt@gmail.com>
2024-01-18 22:57:58 +0100

    Styled top menu

--- app/components/button.tsx
+++ app/components/button.tsx
@@ -14,7 +14,7 @@ function Button({
 		<Link
 			href={href}
 			target={target}
-			className='inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white'>
+			className='inline-flex items-center px-4 py-2 text-sm font-medium border border-color-gray  focus:z-10 focus:ring-2 hover:bg-[rgb(var(--white))] color-black bg-[rgba(var(--white),0.95)]'>
 			{children}
 		</Link>
 	)
