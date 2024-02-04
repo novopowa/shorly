e5c8d94712ade9bb36c8d1207e6aa1cccd8fe007
clau <claudmatt@gmail.com>
2024-02-04 21:41:15 +0100

    Removed spiritual topic fromo wallpapers

--- app/services/unsplash.ts
+++ app/services/unsplash.ts
@@ -10,7 +10,7 @@ const supabase = createServerComponentClient<Database>({ cookies })
 export async function getUnsplashImage(height: number): Promise<string | undefined> {
 	try {
 		const apiURL = `https://api.unsplash.com/photos/random?client_id=${process.env.UNSPLASH_ACCESS_KEY}`
-		const topics = ['iXRd8cmpUDI', 'bo8jQKTaE0Y', '6sMVjTLSkeQ', '_8zFHuhRhyo'] // Cool tones, wallpaper, nature, spirituallity
+		const topics = ['iXRd8cmpUDI', 'bo8jQKTaE0Y', '6sMVjTLSkeQ'] // Cool tones, wallpaper, nature
 		const params = ['count=1', 'orientation=landscape', `topics=${topics.join(',')}`]
 		const url = `${apiURL}&${params.join('&')}`
 		const response = await fetch(url, { next: { revalidate: 300 } }) // Cache wallpaper for 5 min.
