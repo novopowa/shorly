13d46daf1ac882b4830947a7bc667af1309d9a29
clau <claudmatt@gmail.com>
2024-02-04 18:06:12 +0100

    Fixed 'type random' bug from unsplash service

--- app/services/unsplash.ts
+++ app/services/unsplash.ts
@@ -1,7 +1,5 @@
 'use server'
 
-import { type Random } from 'unsplash-js/dist/methods/photos/types'
-
 export async function getUnsplashImage(width: number): Promise<string | undefined> {
 	try {
 		const apiURL = `https://api.unsplash.com/photos/random?client_id=${process.env.UNSPLASH_ACCESS_KEY}`
@@ -9,7 +7,7 @@ export async function getUnsplashImage(width: number): Promise<string | undefine
 		const params = ['count=1', 'orientation=landscape', `topics=${topics.join(',')}`]
 		const url = `${apiURL}&${params.join('&')}`
 		const response = await fetch(url, { next: { revalidate: 300 } }) // Cache wallpaper for 5 min.
-		const images: Random[] = await response.json()
+		const images = await response.json()
 		return `${images[0].urls.raw}&q=75&w=${width}`
 	} catch {
 		return undefined
