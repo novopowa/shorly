4dcabd3700c888f6b2f948a6b2d43c9940a7dfa0
clau <claudmatt@gmail.com>
2024-02-05 16:49:12 +0100

    Changed logo from png to svg

---
+++ app/components/header/logo.tsx
@@ -0,0 +1,19 @@
+export function Logo() {
+	return (
+		<svg
+			className='icon icon-tabler icon-tabler-link rotate-90 mt-[1.5px] ml-[2.1px]'
+			width='60'
+			height='60'
+			viewBox='0 0 24 24'
+			strokeWidth='2'
+			stroke='rgb(var(--black))'
+			fill='none'
+			strokeLinecap='round'
+			strokeLinejoin='round'>
+			<path stroke='none' d='M0 0h24v24H0z' fill='none' />
+			<path d='M9 15l6 -6' stroke='rgb(var(--green))' />
+			<path d='M11 6l.463 -.536a5 5 0 0 1 7.071 7.072l-.534 .464' />
+			<path d='M13 18l-.397 .534a5.068 5.068 0 0 1 -7.127 0a4.972 4.972 0 0 1 0 -7.071l.524 -.463' />
+		</svg>
+	)
+}
--- app/components/header/title.tsx
+++ app/components/header/title.tsx
@@ -1,6 +1,7 @@
 import { Bree_Serif } from 'next/font/google'
-import Image from 'next/image'
 import Link from 'next/link'
+import { Logo } from './logo'
+
 const bree = Bree_Serif({ subsets: ['latin'], weight: '400' })
 
 function Title() {
@@ -8,7 +9,9 @@ function Title() {
 		<Link
 			href='/'
 			className={`flex gap-1 items-center w-50 mt-9 bgcolor-white h-12 rounded-r-lg shadow-[0_0_10px_rgba(0,0,0,0.6)]`}>
-			<Image src='/images/logo.webp' alt='Logo of Shorly' width={80} height={80} className='block h-20 w-20' priority />
+			<div className='h-20 w-20 bgcolor-white border-8 border-[rgb(var(--green))] rounded-full ml-1'>
+				<Logo />
+			</div>
 			<h1 className={`uppercase -mt-1 text-3xl color-black mr-3 ${bree.className}`}>Shorly</h1>
 		</Link>
 	)
