c55308fc7fa11f1b361eaa578191c16919def4d6
clau <claudmatt@gmail.com>
2024-02-05 19:22:56 +0100

    Added title to sign in page

--- app/(pages)/auth/page.tsx
+++ app/(pages)/auth/page.tsx
@@ -4,8 +4,13 @@ import { securePage } from '@/app/utils/session'
 async function Auth() {
 	await securePage('anonymous')
 	return (
-		<div className='flex mx-2 min-[460px]:mx-auto w-auto min-[460px]:w-full max-w-md bgcolor-white rounded-lg p-5 z-10'>
-			<SigninOptions />
+		<div className='mx-2 min-[460px]:mx-auto w-auto min-[460px]:w-full max-w-md bgcolor-white rounded-lg p-5 z-10'>
+			<h2 className='color-black font-bold text-lg mb-4 text-center'>
+				Sign In and Unlock <span className='color-green'>Limitless</span> Link Shortening Opportunities!
+			</h2>
+			<div className='flex '>
+				<SigninOptions />
+			</div>
 		</div>
 	)
 }
