adc089e5bde9595bdcf194df03e29a299ac9412e
clau <claudmatt@gmail.com>
2024-02-09 12:24:47 +0100

    Added visits count to redirection route

--- app/(pages)/[alias]/route.ts
+++ app/(pages)/[alias]/route.ts
@@ -9,18 +9,41 @@ export async function GET(
 	request: NextRequest,
 	context: { params: { alias: string } }
 ): Promise<NextResponse<unknown>> {
+	const supabase = createServerComponentClient<Database>({ cookies })
+
 	const getLinkByAlias = async (alias: string): Promise<LINK> => {
 		'use server'
-		const supabase = createServerComponentClient<Database>({ cookies })
 		const { error, data } = await supabase.from('links').select('*').eq('alias', alias)
-
 		if (error !== null) {
 			notFound()
 		}
 		return data[0] as unknown as LINK
 	}
 
+	const updateVisits = async (linkId: string): Promise<void> => {
+		'use server'
+		const { error, data } = await supabase
+			.from('statistics')
+			.select('id, value')
+			.eq('key', 'visits')
+			.eq('link_id', linkId)
+		if (error !== null) {
+			notFound()
+		} else {
+			const id: string = data === null ? '' : data[0].id
+			const visits: string = data === null ? '0' : data[0].value
+			const newVisits: number = parseInt(visits) + 1
+			const { error } = await supabase
+				.from('statistics')
+				.upsert({ id, key: 'visits', value: newVisits.toString(), link_id: linkId })
+			if (error !== null) {
+				notFound()
+			}
+		}
+	}
+
 	const alias = context.params.alias
 	const link: LINK = await getLinkByAlias(alias)
+	await updateVisits(link.id)
 	return NextResponse.redirect(link.url)
 }
