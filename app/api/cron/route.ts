12c8ad612f31ee64a94cf76598d34bf6ae76f9a3
clau <claudmatt@gmail.com>
2024-02-11 22:24:07 +0100

    Fixed cron secret key bug

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -1,28 +1,30 @@
-import { notFound } from 'next/navigation'
 import { type Database } from '@/app/types/database'
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 import { type NextRequest } from 'next/server'
 
 export function GET(request: NextRequest): Response {
-	if (request.headers.get('Authorization') === `Bearer ${process.env.CRON_SECRET}`) {
-		const deleteAllAnonymousLinks = async (): Promise<void> => {
-			'use server'
-			const supabase = createServerComponentClient<Database>({ cookies })
-			const oneDayAgoDate = new Date()
-			oneDayAgoDate.setDate(new Date().getDate() - 1)
-			const { error } = await supabase
-				.from('links')
-				.delete()
-				.lt('created_at', `${oneDayAgoDate.toISOString().replace('T', ' ')}+00`)
-				.is('user_id', null)
-			if (error !== null) {
-				notFound()
-			}
-		}
-		deleteAllAnonymousLinks()
-		return Response.json({})
-	} else {
-		return Response.json({ error: 'SECRET KEY NEEDED' })
+	const authHeader = request.headers.get('authorization')
+	if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+		return new Response('Unauthorized', {
+			status: 401
+		})
 	}
+
+	const deleteAllAnonymousLinks = async (): Promise<void> => {
+		'use server'
+		const supabase = createServerComponentClient<Database>({ cookies })
+		const oneDayAgoDate = new Date()
+		oneDayAgoDate.setDate(new Date().getDate() - 1)
+		const { error } = await supabase
+			.from('links')
+			.delete()
+			.lt('created_at', `${oneDayAgoDate.toISOString().replace('T', ' ')}+00`)
+			.is('user_id', null)
+		if (error !== null) {
+			throw new Error(error as unknown as string)
+		}
+	}
+	deleteAllAnonymousLinks()
+	return Response.json({})
 }
